0fb17c0afc5be7a32eda3ecf09ecd5052a343cac
Kevin11ts <20233l001135@utcv.edu.mx>
2025-03-08 14:13:55 -0600

    refactor(config/db.js: Correción en la conexión de la api a Mongo Atlas

--- config/db.js
+++ config/db.js
@@ -8,25 +8,29 @@ const { Pool } = pg;
 
 // Conexión a PostgreSQL
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 // Conexión a MongoDB
-const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/arduinoDB";
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://kevin11:kevin11@cluster0.znfj1.mongodb.net/arduinoProyecto?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(mongoURI);
-
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((error) => console.error("Error al conectar a MongoDB:", error));
 
 const mongoDB = mongoose.connection;
+
+// Manejo de errores de conexión a MongoDB
 mongoDB.on("error", console.error.bind(console, "Error de conexión a MongoDB:"));
 mongoDB.once("open", () => {
-    console.log("Conectado a MongoDB");
+  console.log("Conexión a MongoDB establecida");
 });
 
-// Exportaciones correctas
-export default pool;
-export { mongoDB };
+// Exportaciones
+export default pool; // Para PostgreSQL
+export { mongoDB }; // Para MongoDB
