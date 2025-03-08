278da5b5bed54a5a6f0873799d298cd5d27ee6fc
Kevin11ts <20233l001135@utcv.edu.mx>
2025-03-08 14:30:41 -0600

    feat(routes/datos/datosRoutes.js): Correción en las rutas ajustadas de acuerdo al nuevo esquema de Data

--- routes/datos/datosRoutes.js
+++ routes/datos/datosRoutes.js
@@ -1,52 +1,58 @@
-import express from "express";
-import { recibirDatos, obtenerDatos } from "../../controllers/datos/datosController.js";
+import express from 'express';
+import DataController from '../../controllers/datos/datosController.js';
 
 const router = express.Router();
 
 /**
  * @swagger
  * tags:
- *   name: Datos
- *   description: Endpoints para el manejo de datos
+ *   name: data
  */
 
 /**
  * @swagger
- * /datos:
+ * /api/data:
+ *   get:
+ *     summary: Obtener todos los registros de datos
+ *     tags: [data]
+ *     responses:
+ *       200:
+ *         description: Lista de todos los registros de datos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/data'
+ *       500:
+ *         description: Error interno del servidor
+ */
+router.get("/data", DataController.getAll);
+
+/**
+ * @swagger
+ * /api/data:
  *   post:
- *     summary: Guarda datos enviados por Arduino
- *     tags: 
- *       - Datos
+ *     summary: Crear un nuevo registro de datos
+ *     tags: [data]
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             type: object
- *             properties:
- *               elemento:
- *                 type: string
- *               valor:
- *                 type: number
- *               unidades:
- *                 type: string
+ *             $ref: '#/components/schemas/data'
  *     responses:
  *       201:
- *         description: Datos almacenados correctamente.
+ *         description: Registro de datos creado con éxito
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/data'
+ *       400:
+ *         description: Solicitud incorrecta, datos inválidos
+ *       500:
+ *         description: Error interno del servidor
  */
-router.post("/", recibirDatos);
-
-/**
- * @swagger
- * /datos:
- *   get:
- *     summary: Obtiene los datos almacenados
- *     tags: 
- *       - Datos
- *     responses:
- *       200:
- *         description: Datos obtenidos correctamente.
- */
-router.get("/", obtenerDatos);
+router.post("/data", DataController.create);
 
 export default router;
